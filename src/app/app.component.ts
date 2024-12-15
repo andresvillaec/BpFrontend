7e2157121606c9036f564c2a5fda6cdd9ad40a65
Andres Villavicencio <avillavicencio@devsu.com>
2024-12-15 16:43:56 -0500

    feat: Remove useless references

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { ContentComponent } from "./shared/components/content/content.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, ContentComponent],
+  imports: [HeaderComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterOutlet],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
