657f1afc0552c52ade121d1ab6fb5d4b0f09952b
Andres Villavicencio <avillavicencio@devsu.com>
2024-12-15 16:40:24 -0500

    feat: Set up content by route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/components/header/header.component";
 import { ContentComponent } from "./shared/components/content/content.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ContentComponent],
+  imports: [RouterOutlet, HeaderComponent, ContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
