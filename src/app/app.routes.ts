ed15955ed447c1b58b63cdf8a431b0efffcc5138
Andres Villavicencio <avillavicencio@devsu.com>
2024-12-15 15:56:32 -0500

    feat: Set routes for product CRUD

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from "./features/products/product-list/product-list.component";
+import { ProductCreateComponent } from "./features/products/product-create/product-create.component";
+import { ProductEditComponent } from "./features/products/product-edit/product-edit.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Listado de productos',
+    component: ProductListComponent,
+  },
+  {
+    path: 'products',
+    title: 'Listado de productos',
+    component: ProductListComponent,
+  },
+  {
+    path: 'products/create',
+    title: 'Crear producto',
+    component: ProductCreateComponent,
+  },
+  {
+    path: 'products/:id',
+    title: 'Editar producto',
+    component: ProductEditComponent,
+  },
+];
