86ff10ed0450c66abfa373fcb29e37b7708fa500
Andres Villavicencio <villadosa@hotmail.com>
2024-12-14 23:28:56 -0500

    feat: Change title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,13 +17,13 @@ describe('AppComponent', () => {
   it(`should have the 'BpFrontend' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('BpFrontend');
+    expect(app.title).toEqual('Andres Villavicencio BpFrontend');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, BpFrontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Andres Villavicencio BpFrontend');
   });
 });
