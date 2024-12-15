6dcc60f7c9492e290f7088728a6fc86dc6a21090
Andres Villavicencio <avillavicencio@devsu.com>
2024-12-15 17:37:29 -0500

    feat: Remove useless statement on product list component

--- src/app/features/products/product-list/product-list.component.ts
+++ src/app/features/products/product-list/product-list.component.ts
@@ -7,7 +7,6 @@ import { RouterLink, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-product-list',
-  standalone: true,
   imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.scss'
